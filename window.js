40b12afb57150176c46ee5aabb750c25677db8ed
Jinrui <jinrui@kooboo.cn>
2023-08-11 15:28:35 +0800

    动态设置 window 的显示参数

--- window.js
+++ window.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     // 预加载脚本
     webPreferences: {
       preload: path.resolve(__dirname, 'preload.js'),
-      sandbox: false  // 预加载脚本可以使用 fs、electron.shell 等，渲染进程不能使用
+      // sandbox: false 预加载脚本可以使用 fs、electron.shell 等，渲染进程不能使用
       // contextIsolation: false 当不进行隔离时，预加载脚本 preload.js 的内容可直接被渲染进程调用
       // nodeIntegration: true 当开启此配置，预加载脚本可以使用 fs、electron.shell
       // 当两个配置都设置时，渲染脚本也可以使用
@@ -19,6 +19,16 @@ const createWindow = () => {
 
   mainWindow.webContents.openDevTools()
   mainWindow.loadFile(path.resolve(__dirname, 'index.html'))
+
+  // setTimeout(() => {
+  //   mainWindow.center()
+  //   mainWindow.setBounds({
+  //     width: 600,
+  //     height: 600,
+  //     x: 0,
+  //     y: 0
+  //   }, true)
+  // }, 2000)
 
   return mainWindow
 }
