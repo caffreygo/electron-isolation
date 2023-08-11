b0557e59f849f55001bc15b869b284e1a659c3bb
Jerry <jerry.chenj@outlook.com>
2023-08-11 16:38:47 +0800

    Mac 关闭不退出（保留在 docker 栏），再点击时开启

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ app.whenReady().then(() => {
   createWindow()
 })
 
+// 行为设置：Mac 关闭不退出（保留在 docker 栏），再点击时开启
+app.on('window-all-closed', () => {
+  // 如果不是苹果，退出应用
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0 && process.platform === 'darwin') {
+    createWindow()
+  }
+})
+
 ipcMain.on('setWindowPosition', (event, options) => {
   const win = BrowserWindow.fromWebContents(event.sender)
 
